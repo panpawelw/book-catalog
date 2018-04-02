3e82cb729b2c88cb24771409da3f4f8e37f3d4be
pjm77 <probably.just.me77@gmail.com>
2018-04-02 18:16:41 +0200

    Jackson library added and tested

--- src/main/java/pl/pjm77/app/BookController.java
+++ src/main/java/pl/pjm77/app/BookController.java
@@ -11,4 +11,10 @@ public class BookController {
 	public String hello() {
 		return "{hello:world}";
 	}
+	
+	@RequestMapping("/helloBook")
+	public Book helloBook() {
+		return new Book(1L,"9788324631766","Thinking in Java",
+				"Bruce Eckel","Helion","programming");
+	}
 }
