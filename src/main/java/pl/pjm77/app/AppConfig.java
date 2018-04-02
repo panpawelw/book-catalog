cf92b28a9821781893ad395398776d39eebd4ccc
pjm77 <probably.just.me77@gmail.com>
2018-04-02 02:16:28 +0200

    project setup

---
+++ src/main/java/pl/pjm77/app/AppConfig.java
@@ -0,0 +1,12 @@
+package pl.pjm77.app;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "pl.pjm77")
+public class AppConfig extends WebMvcConfigurerAdapter {
+}
