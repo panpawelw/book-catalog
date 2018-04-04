b4e2e26b0c7175ac0827811b12c99826f2751f14
pjm77 <probably.just.me77@gmail.com>
2018-04-04 19:11:31 +0200

    Get list of all books method implemented

--- src/main/java/pl/pjm77/app/BookController.java
+++ src/main/java/pl/pjm77/app/BookController.java
@@ -1,12 +1,29 @@
 package pl.pjm77.app;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/books")
 public class BookController {
 
+	public MemoryBookService memoryBookService;
+	
+	@Autowired
+	public BookController(MemoryBookService memoryBookService) {
+		this.memoryBookService = memoryBookService;
+	}
+	
+	@RequestMapping("/")
+	@ResponseBody
+	public List<Book >getBooks() {
+		return memoryBookService.getList();
+	}
+	
 	@RequestMapping("/hello")
 	public String hello() {
 		return "{hello:world}";
--- src/main/webapp/resources/js/app.js
+++ src/main/webapp/resources/js/app.js
@@ -4,12 +4,13 @@ $(document).ready(function(){
 		$('.testButton').on('click', function(event){
 			var books;
 			$.ajax({
-				url: 'http://localhost:8080/Workshop_4and5/books/helloBook',
+				url: 'http://localhost:8080/Workshop_4and5/books/',
 				data: {books: books},
 				type: 'GET',
 				dataType: 'json'})
 				.done(function(result){
-					alert(result.id + '\n' + result.isbn + '\n' + result.title +'\n' + result.author + '\n' + result.publisher + '\n' + result.type);
+					alert(result[0].id + '\n' + result[0].isbn + '\n' + result[0].title +'\n' + result[0].author + '\n' + result[0].publisher + '\n' + result[0].type +
+							'\n' + result[1].id + '\n' + result[2].id);
 					})
 				.fail(function(){alert('Error!')})
 				.always(function(){alert('Always!')});
