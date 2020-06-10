9ae0d95a0ced7c7f9c6d1d857d517505bc97607f
Paweł Wojtuń <panpawelw@pm.me>
2020-06-10 15:00:51 +0200

    Add getBookById method

--- src/main/java/pl/pjm77/app/DatabaseBookService.java
+++ src/main/java/pl/pjm77/app/DatabaseBookService.java
@@ -12,29 +12,27 @@ import org.springframework.stereotype.Service;
 @Service
 public class DatabaseBookService implements BookService {
 
-    private DataSource dataSource;
-    private JdbcTemplate jdbcTemplate;
+    private final JdbcTemplate jdbcTemplate;
 
     @Autowired
     public DatabaseBookService(DataSource dataSource) {
-        this.dataSource = dataSource;
         jdbcTemplate = new JdbcTemplate(dataSource);
         jdbcTemplate.execute("DROP TABLE IF EXISTS books");
         jdbcTemplate.execute("CREATE TABLE IF NOT EXISTS books (id BIGINT AUTO_INCREMENT NOT " +
           "NULL, isbn VARCHAR(255), title VARCHAR(255), author VARCHAR(255), publisher VARCHAR" +
           "(255), type VARCHAR(255), PRIMARY KEY(id));");
 
-        this.addBook(new Book("9788324631766", "Thinking in Java.", "Bruce Eckel",
+        this.addBook(new Book("9788324631766", "Thinking in Java", "Bruce Eckel",
           "Helion", "programming"));
-        this.addBook(new Book("9788324627738", "Rusz glowa, Java.",
+        this.addBook(new Book("9788324627738", "Rusz glowa, Java",
           "Sierra Kathy, Bates Bert", "Helion", "programming"));
         this.addBook(new Book("9780130819338", "Java 2. Podstawy.",
           "Cay Horstmann, Gary Cornell", "Helion", "programming"));
-        this.addBook(new Book("9781932394856", "Test Driven.", "Lance Koskela",
+        this.addBook(new Book("9781932394856", "Test Driven", "Lance Koskela",
           "Manning", "programming"));
-        this.addBook(new Book("9780134685991", "Effective Java.", "Joshua Bloch",
+        this.addBook(new Book("9780134685991", "Effective Java", "Joshua Bloch",
           "Addison - Wesley Professional", "programming"));
-        this.addBook(new Book("9780132350884", "Clean Code.", "Robert C. Martin",
+        this.addBook(new Book("9780132350884", "Clean Code", "Robert C. Martin",
           "Prentice Hall", "programming"));
         this.addBook(new Book("9780596007126", "Head First. Design Patterns",
           "Eric Freeman, Bert Bates. Kathy Sierra, Elisabeth Robson", "O'Reilly",
@@ -50,20 +48,28 @@ public class DatabaseBookService implements BookService {
 
     @Override
     public boolean updateBook(Book book) {
-        return jdbcTemplate.update("UPDATE book SET isbn=?, title=?, author=?, publisher=?, " +
-          "type=?", book.getIsbn(), book.getTitle(), book.getAuthor(), book.getPublisher(),
-          book.getType()) == 1;
+        return jdbcTemplate.update("UPDATE books SET isbn=?, title=?, author=?, publisher=?, " +
+          "type=? WHERE id=?", book.getIsbn(), book.getTitle(), book.getAuthor(),
+          book.getPublisher(), book.getType(), book.getId()) == 1;
     }
 
     @Override
     public boolean deleteBook(long id) {
-        String sql = "DELETE FROM books WHERE id = ?";
-        return jdbcTemplate.update(sql, new Object[] {id}) == 1;
+        return jdbcTemplate.update("DELETE FROM books WHERE id=?", id) == 1;
     }
 
     @Override
     public Book getBookById(long id) {
-        return null;
+        return jdbcTemplate.queryForObject("SELECT * FROM books WHERE id=?",
+          new Object[]{id}, (rs, rowNum) ->
+          new Book(
+            rs.getLong("id"),
+            rs.getString("isbn"),
+            rs.getString("title"),
+            rs.getString("author"),
+            rs.getString("publisher"),
+            rs.getString("type")
+          ));
     }
 
     @Override
