605f7ade6cecc4502a0a6554dc0503f8e07c5513
Paweł Wojtuń <panpawelw@pm.me>
2021-02-22 23:47:33 +0100

    Add basic test lists in *BookServiceTests classes

--- src/test/java/MemoryBookServiceTests.java
+++ src/test/java/MemoryBookServiceTests.java
@@ -1,2 +1,22 @@
+import org.junit.Test;
+
 public class MemoryBookServiceTests {
+
+    @Test
+    public void populateDatabaseTest() {}
+
+    @Test
+    public void addBookTest() {}
+
+    @Test
+    public void updateBookTest() {}
+
+    @Test
+    public void deleteBookTest() {}
+
+    @Test
+    public void getBookByIdTest() {}
+
+    @Test
+    public void getBooksTest() {}
 }
