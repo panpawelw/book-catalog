0c091c98736af0ebc3c33f47602e2d01de3fc17c
Paweł Wojtuń <panpawelw@pm.me>
2021-03-13 00:00:10 +0100

    Add tests to BookControllerTests

--- src/test/java/BookControllerTests.java
+++ src/test/java/BookControllerTests.java
@@ -1,15 +1,23 @@
 import com.panpawelw.bookcatalog.Book;
-import com.panpawelw.bookcatalog.BookService;
+import com.panpawelw.bookcatalog.BookController;
+import com.panpawelw.bookcatalog.MemoryBookService;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.context.ApplicationContext;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import static com.panpawelw.bookcatalog.Misc.getBooksAsMap;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -19,35 +27,73 @@ public class BookControllerTests {
       "test author", "test publishers", "test type");
 
   @Mock
-  private BookService service;
+  private ApplicationContext context;
+
+  @Mock
+  private MemoryBookService service;
+
+  @Mock
+  private HttpServletRequest request;
+
+  @Mock
+  private HttpServletResponse response;
+
+  private BookController bookController;
+
+  private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+  private final PrintStream originalOut = System.out;
+
+
+  @Before
+  public void setup() {
+    System.setOut(new PrintStream(outContent));
+    bookController = new BookController(service, context);
+  }
+
+  @After
+  public void cleanUp() {
+    System.setOut(originalOut);
+  }
 
   @Test
-  public void getBooksTest(){
+  public void getBooksTest() {
     when(service.getBooks()).thenReturn(getBooksAsMap());
-    assertEquals(service.getBooks(), getBooksAsMap());
+    assertEquals(bookController.getBooks(), getBooksAsMap());
   }
 
   @Test
-  public void getBookByIdTest(){
-    when(service.getBookById(anyLong())).thenReturn(TEST_BOOK);
-    assertEquals(service.getBookById(3), TEST_BOOK);
+  public void getBookByIdTest() {
+    when(service.getBookById(3)).thenReturn(TEST_BOOK);
+    assertEquals(bookController.getBookById(3), TEST_BOOK);
   }
 
   @Test
-  public void addBookTest(){
+  public void addBookWithCorrectBookTest() {
     when(service.addBook(any())).thenReturn(true);
-    assertTrue(service.addBook(TEST_BOOK));
+    bookController.addBook(TEST_BOOK.getIsbn(), TEST_BOOK.getTitle(), TEST_BOOK.getAuthor(),
+        TEST_BOOK.getPublisher(), TEST_BOOK.getType(), request, response);
+    assertEquals("", outContent.toString());
   }
 
   @Test
-  public void updateBookTest(){
-    when(service.updateBook(anyLong(), any())).thenReturn(true);
-    assertTrue(service.updateBook(1, TEST_BOOK));
+  public void addBookWithIncorrectBookTest() {
+    when(service.addBook(any())).thenReturn(false);
+    bookController.addBook(TEST_BOOK.getIsbn(), TEST_BOOK.getTitle(), TEST_BOOK.getAuthor(),
+        TEST_BOOK.getPublisher(), TEST_BOOK.getType(), request, response);
+    assertEquals("Error adding book!\r\n", outContent.toString());
   }
 
   @Test
-  public void deleteBookTest(){
-    when(service.deleteBook(anyLong())).thenReturn(true);
-    assertTrue(service.deleteBook(1));
+  public void updateBookTest() {
+    when(service.updateBook(1, TEST_BOOK)).thenReturn(true);
+    service.updateBook(1, TEST_BOOK);
+    assertEquals("", outContent.toString());
+  }
+
+  @Test
+  public void deleteBookTest() {
+    when(service.deleteBook(1)).thenReturn(true);
+    service.deleteBook(1);
+    assertEquals("", outContent.toString());
   }
 }
