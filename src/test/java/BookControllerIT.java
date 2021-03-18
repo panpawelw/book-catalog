d30f8348ca591f3a150764d62157cbaae49a1a4c
Paweł Wojtuń <panpawelw@pm.me>
2021-03-19 00:59:42 +0100

    Finish getBooksTest()

--- src/test/java/BookControllerIT.java
+++ src/test/java/BookControllerIT.java
@@ -1,4 +1,8 @@
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.panpawelw.bookcatalog.Book;
 import com.panpawelw.bookcatalog.BookController;
+import com.panpawelw.bookcatalog.Misc;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,7 +12,11 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
+import java.util.HashMap;
+
+import static org.junit.Assert.assertEquals;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -30,7 +38,43 @@ public class BookControllerIT {
 
   @Test
   public void getBooksTest() throws Exception {
-    mockMvc.perform(get("/getallbooks").contentType(MediaType.APPLICATION_JSON))
-        .andExpect(status().isOk());
+    mockMvc.perform(get("/resetdatabase")).andExpect(status().isOk());
+    MvcResult result = mockMvc.perform(get("/getallbooks")
+        .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk()).andReturn();
+    ObjectMapper mapper = new ObjectMapper();
+    HashMap<Long, Book> bookList = mapper.readValue(result.getResponse().getContentAsString(),
+        new TypeReference<HashMap<Long, Book>>() {});
+    assertEquals(bookList, Misc.getBooksAsMap());
+  }
+
+  @Test
+  public void addBookTest() throws Exception {
+
+  }
+
+  @Test
+  public void updateBookTest() throws Exception {
+
+  }
+
+  @Test
+  public void deleteBookTest() throws Exception {
+
+  }
+
+  @Test
+  public void switchToMySQLDatabaseTest() throws Exception {
+
+  }
+
+  @Test
+  public void switchToMemoryDatabaseTest() throws Exception {
+
+  }
+
+  @Test
+  public void resetDatabaseTest() throws Exception {
+
   }
 }
