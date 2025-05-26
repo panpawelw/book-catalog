c71ee87e48ca030b68f029b516d32e63f83df1c3
Paweł Wojtuń <panpawelw@protonmail.com>
2025-05-26 16:41:27 +0200

    Update broken dependencies

--- src/main/java/com/panpawelw/bookcatalog/AppInitializer.java
+++ src/main/java/com/panpawelw/bookcatalog/AppInitializer.java
@@ -1,6 +1,6 @@
 package com.panpawelw.bookcatalog;
 
-import javax.servlet.Filter;
+import jakarta.servlet.Filter;
 
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
