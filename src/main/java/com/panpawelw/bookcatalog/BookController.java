35a71d33b4805330cbf38468f2d0165e098d2b79
Paweł Wojtuń <panpawelw@protonmail.com>
2025-05-31 12:47:35 +0200

    fix: update imports

--- src/main/java/com/panpawelw/bookcatalog/BookController.java
+++ src/main/java/com/panpawelw/bookcatalog/BookController.java
@@ -3,8 +3,8 @@ package com.panpawelw.bookcatalog;
 import java.io.IOException;
 import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
