414113ca40cb6c36b888fb25633ac9313d754a20
Paweł Wojtuń <panpawelw@protonmail.com>
2025-06-05 23:29:51 +0200

    refactor: rename variable

--- src/main/java/com/panpawelw/bookcatalog/MemoryBookService.java
+++ src/main/java/com/panpawelw/bookcatalog/MemoryBookService.java
@@ -8,19 +8,19 @@ import org.springframework.stereotype.Service;
 @Service
 public class MemoryBookService implements BookService {
 
-  private final Map<Long, Book> list;
+  private final Map<Long, Book> bookList;
   private long nextId;
 
   public MemoryBookService() {
-    list = new HashMap<>();
+    bookList = new HashMap<>();
     populateDatabase();
   }
 
   public void populateDatabase() {
     nextId = 0;
-    list.clear();
+    bookList.clear();
     for (Book book : Misc.BOOK_LIST) {
-      list.put(getNextFreeId(), book);
+      bookList.put(getNextFreeId(), book);
     }
   }
 
@@ -32,28 +32,28 @@ public class MemoryBookService implements BookService {
   public boolean addBook(Book book) {
     long idCounter = getNextFreeId();
     book.setId(idCounter);
-    return list.put(idCounter, book) == null;
+    return bookList.put(idCounter, book) == null;
   }
 
   @Override
   public boolean updateBook(long id, Book book) {
     if (book == null || id <= 0) return false;
-    return list.replace(id, book) != null;
+    return bookList.replace(id, book) != null;
   }
 
   @Override
   public boolean deleteBook(long id) {
-    if (id <= 0 || list.get(id) == null) return false;
-    return list.remove(id) != null;
+    if (id <= 0 || bookList.get(id) == null) return false;
+    return bookList.remove(id) != null;
   }
 
   @Override
   public Book getBookById(long id) {
-    return list.get(id);
+    return bookList.get(id);
   }
 
   @Override
   public Map<Long, Book> getBooks() {
-    return list;
+    return bookList;
   }
 }
